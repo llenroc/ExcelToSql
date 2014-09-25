051d668e8b7e11d6987fbba11d2047e2a04f1a5d
messboy home <messboy000@gmail.com>
2014-09-25 23:12:53 +0800

    初版

---
+++ ExcelToSql/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace ExcelToSql
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtbSourceFile = new System.Windows.Forms.TextBox();
+            this.cbIsRowType = new System.Windows.Forms.CheckBox();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.btnImportData = new System.Windows.Forms.Button();
+            this.txtProjectName = new System.Windows.Forms.TextBox();
+            this.ProjectName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtbSourceFile
+            // 
+            this.txtbSourceFile.Location = new System.Drawing.Point(44, 24);
+            this.txtbSourceFile.Name = "txtbSourceFile";
+            this.txtbSourceFile.Size = new System.Drawing.Size(440, 22);
+            this.txtbSourceFile.TabIndex = 0;
+            // 
+            // cbIsRowType
+            // 
+            this.cbIsRowType.AutoSize = true;
+            this.cbIsRowType.Location = new System.Drawing.Point(383, 67);
+            this.cbIsRowType.Name = "cbIsRowType";
+            this.cbIsRowType.Size = new System.Drawing.Size(101, 16);
+            this.cbIsRowType.TabIndex = 1;
+            this.cbIsRowType.Text = "TypeGuessRows";
+            this.cbIsRowType.UseVisualStyleBackColor = true;
+            this.cbIsRowType.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(512, 22);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenFile.TabIndex = 2;
+            this.btnOpenFile.Text = "開啟資料";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // btnImportData
+            // 
+            this.btnImportData.Location = new System.Drawing.Point(512, 63);
+            this.btnImportData.Name = "btnImportData";
+            this.btnImportData.Size = new System.Drawing.Size(75, 23);
+            this.btnImportData.TabIndex = 3;
+            this.btnImportData.Text = "匯入資料";
+            this.btnImportData.UseVisualStyleBackColor = true;
+            this.btnImportData.Click += new System.EventHandler(this.btnImportData_Click);
+            // 
+            // txtProjectName
+            // 
+            this.txtProjectName.Location = new System.Drawing.Point(116, 58);
+            this.txtProjectName.Name = "txtProjectName";
+            this.txtProjectName.Size = new System.Drawing.Size(100, 22);
+            this.txtProjectName.TabIndex = 4;
+            // 
+            // ProjectName
+            // 
+            this.ProjectName.AutoSize = true;
+            this.ProjectName.Font = new System.Drawing.Font("新細明體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.ProjectName.Location = new System.Drawing.Point(44, 61);
+            this.ProjectName.Name = "ProjectName";
+            this.ProjectName.Size = new System.Drawing.Size(67, 15);
+            this.ProjectName.TabIndex = 5;
+            this.ProjectName.Text = "專案名稱";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(630, 125);
+            this.Controls.Add(this.ProjectName);
+            this.Controls.Add(this.txtProjectName);
+            this.Controls.Add(this.btnImportData);
+            this.Controls.Add(this.btnOpenFile);
+            this.Controls.Add(this.cbIsRowType);
+            this.Controls.Add(this.txtbSourceFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtbSourceFile;
+        private System.Windows.Forms.CheckBox cbIsRowType;
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.Button btnImportData;
+        private System.Windows.Forms.TextBox txtProjectName;
+        private System.Windows.Forms.Label ProjectName;
+    }
+}
+
---
+++ ExcelToSql/Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace ExcelToSql
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "Excel 活頁簿 (*.xlsx)|*.xlsx|Excel 97-2003 (*.xls)|*.xls|文字檔 (Tab 字元分隔) (*.txt)|*.txt";
+                if (ofd.ShowDialog() == DialogResult.OK)
+                    txtbSourceFile.Text = ofd.FileName;
+                else
+                    txtbSourceFile.Text = string.Empty;
+            }
+        }
+
+        /// <summary>
+        /// 取得 Excel 文件中指定工作表的內容
+        /// </summary>
+        /// <param name="FileFullPath">檔案路徑</param>
+        /// <param name="SheetName">工作表名稱</param>
+        /// <returns>DataTable 工作表內容</returns>
+        private DataTable GetExcelSheetData(string FileFullPath, string SheetName)
+        {
+            //string strConn = "Provider=Microsoft.Jet.OleDb.4.0;" + "data source=" + FileFullPath + ";Extended Properties='Excel 8.0; HDR=NO; IMEX=1'"; //此連接只能操作Excel2007之前(.xls)文件
+            string strConn = "Provider=Microsoft.Ace.OleDb.12.0;" + "data source=" + FileFullPath + ";Extended Properties='Excel 12.0; HDR=YES; IMEX=1'"; //此連接可以操作.xls與.xlsx文件
+            using (OleDbConnection conn = new OleDbConnection(strConn))
+            {
+                conn.Open();
+                DataSet ds = new DataSet();
+                //   ("select * from [Sheet1$]", conn);
+                OleDbDataAdapter odda = new OleDbDataAdapter(string.Format("SELECT * FROM [{0}]", SheetName), conn);
+                odda.Fill(ds, SheetName);
+                return ds.Tables[0];
+            }
+        }
+
+
+        /// <summary>
+        /// 取得 Excel 文件中所有工作表名
+        /// </summary>
+        /// <param name="excelFile">Excel 檔案路徑</param>
+        /// <returns>string[] 工作表名稱集合</returns>
+        private String[] GetExcelSheetNames(string excelFile)
+        {
+            System.Data.DataTable dt = null;
+            try
+            {
+                //string connString = "Provider=Microsoft.Jet.OleDb.4.0;" + "data source=" + excelFile + ";Extended Properties='Excel 8.0; HDR=NO; IMEX=1'"; //此連接只能操作Excel2007之前(.xls)文件
+                string connString = "Provider=Microsoft.Ace.OleDb.12.0;" + "data source=" + excelFile + ";Extended Properties='Excel 12.0; HDR=YES; IMEX=1'"; //此連接可以操作.xls與.xlsx文件
+
+                // 建立連結
+                using (OleDbConnection objConn = new OleDbConnection(connString))
+                {
+                    objConn.Open();
+
+                    // 取得 Excel 資料結構
+                    dt = objConn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+                    if (dt == null)
+                        return null;
+
+                    // 取得 Excel 資料表中的工作表
+                    var excelSheets = dt.AsEnumerable().Where(r => r["TABLE_NAME"].ToString().Contains("$")).Select(s => s["TABLE_NAME"].ToString()).ToArray();
+                    return excelSheets;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            if (dt != null)
+                dt.Dispose();
+        }
+
+        private struct RegExcel
+        {
+            public string Path;
+            public string DefaultValue;
+            public bool Exist;
+
+            public RegExcel(string path, string value, bool exist)
+            {
+                this.Path = path;
+                this.DefaultValue = value;
+                this.Exist = exist;
+            }
+        };
+
+        private void btnImportData_Click(object sender, EventArgs e)
+        {
+            if (txtbSourceFile.Text != "" && txtProjectName.Text != "")
+            {
+                string[][] stringArr = null;
+                RegExcel[] RegExcelPath = {new RegExcel(@"SOFTWARE\Microsoft\Jet\3.5\Engines\Excel" ,"",false)
+                                                , new RegExcel(@"SOFTWARE\Microsoft\Jet\4.0\Engines\Excel","",false)
+                                                , new RegExcel(@"SOFTWARE\Wow6432Node\Microsoft\Jet\4.0\Engines\Excel","",false)
+                                                , new RegExcel(@"SOFTWARE\Wow6432Node\Microsoft\Office\14.0\Access Connectivity Engine\Engines\Excel","",false)
+                                                , new RegExcel(@"SOFTWARE\Wow6432Node\Microsoft\Office\12.0\Access Connectivity Engine\Engines\Excel","",false)
+                                                , new RegExcel(@"SOFTWARE\Microsoft\Office\14.0\Access Connectivity Engine\Engines\Excel","",false) };
+                string[] SheetName = GetExcelSheetNames(txtbSourceFile.Text);
+                switch (Path.GetExtension(txtbSourceFile.Text))
+                {
+                    case ".xls":
+                    case ".xlsx":
+                        if (cbIsRowType.Checked)
+                        {
+                            for (int i = 0; i < RegExcelPath.Length; i++)
+                            {
+                                using (RegistryKey myKey = Registry.LocalMachine.OpenSubKey(RegExcelPath[i].Path, true))
+                                {
+                                    if (myKey != null)
+                                    //檢查子機碼是否存在，檢查資料夾是否存在。
+                                    {
+                                        //若目錄存在，則取出 key=cnstr 的值。
+                                        RegExcelPath[i].DefaultValue = myKey.GetValue("TypeGuessRows").ToString();
+                                        RegExcelPath[i].Exist = true;
+                                        myKey.SetValue("TypeGuessRows", "0", RegistryValueKind.DWord);
+                                    }
+                                }
+                            }
+                        }
+                        DataTable dataTable = GetExcelSheetData(txtbSourceFile.Text, SheetName[0]);
+                        stringArr = dataTable.AsEnumerable().Select(r => r.ItemArray.Select(ra => ra.ToString()).ToArray()).ToArray();
+                        dataTable.Dispose();
+                        break;
+                    case ".txt":
+                        using (StreamReader sr = new StreamReader(txtbSourceFile.Text, Encoding.Default))
+                        {
+                            sr.ReadLine();//去掉標題列
+                            stringArr = (sr.ReadToEnd().Split(new string[] { "\r\n" }, StringSplitOptions.None)).ToArray().Select(r => r.Trim().Split('\t')).ToArray();
+                        }
+                        break;
+                }
+                if (stringArr.Length <= 0)
+                {
+                    MessageBox.Show("匯入資料不符合格式，請重新選擇檔案進行匯入！");
+                    return;
+                }
+                // Excel 資料欄位至少大於2欄
+                if (stringArr[0].Length < 2)
+                {
+                    MessageBox.Show("匯入資料不符合格式，請重新選擇檔案進行匯入！");
+                    return;
+                }
+
+                string strSQL = "";
+                //匯入資料庫前，將資料表清空
+                //string strSQL = "Truncate Table [Northwind].[dbo].[Categories];";
+                for (int i = 0; i < stringArr.Length; i++)
+                {
+                    //自行設定參數
+                    string Project,Category, Priority,Item,Describe,Todo,Owner,Status,StartDate,EndDate,PredictTime_hr;
+                    string[] temp = stringArr[i];
+                    Project = txtProjectName.Text;
+                    Category = temp[1].Trim();
+                    Priority = temp[2].Trim();
+                    Item = temp[3].Trim();
+                    Describe = temp[4].Trim();
+                    Todo = temp[5].Trim();
+                    Owner = temp[6].Trim();
+                    Status = temp[7].Trim();
+                    StartDate = temp[8].Trim();
+                    EndDate = temp[9].Trim();
+                    PredictTime_hr = temp[10].Trim();
+                    strSQL += @"INSERT INTO [WBS].[dbo].[WBS]
+                                ([Project]
+                                ,[Category]
+                                ,[Priority]
+                                ,[Item]
+                                ,[Describe]
+                                ,[Todo]
+                                ,[Owner]
+                                ,[Status]
+                                ,[StartDate]
+                                ,[EndDate]
+                                ,[PredictTime_hr])
+                                VALUES('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}','{8}','{9}','{10}');";
+                    strSQL = string.Format(strSQL, Project, Category, Priority, Item, Describe, Todo, Owner, Status, StartDate, EndDate, PredictTime_hr);
+                }
+
+                using (SqlConnection conn = new SqlConnection("Data Source=localhost;Initial Catalog=WBS;Integrated Security=True"))
+                {
+                    conn.Open();
+                    SqlTransaction trans = conn.BeginTransaction();
+                    SqlCommand cmd = new SqlCommand(strSQL, conn);
+                    cmd.Transaction = trans;
+                    int sqlcount = 0;
+                    try
+                    {
+                        sqlcount = cmd.ExecuteNonQuery();
+                        trans.Commit();
+                        if (sqlcount == stringArr.Length)
+                            MessageBox.Show("匯入資料成功");
+                    }
+                    catch (Exception)
+                    {
+                        trans.Rollback();
+                        MessageBox.Show("匯入資料中 " + (stringArr.Length - sqlcount) + " 筆資料失敗");
+                        throw;
+                    }
+                }
+
+                //回復登錄檔的設定
+                if (cbIsRowType.Checked)
+                {
+                    var ReDefaultReg = RegExcelPath.AsEnumerable().Where(r => r.Exist == true);
+                    foreach (var p in ReDefaultReg)
+                    {
+                        using (RegistryKey myKey = Registry.LocalMachine.OpenSubKey(p.Path, true))
+                        {
+                            myKey.SetValue("TypeGuessRows", p.DefaultValue, RegistryValueKind.DWord);
+                        }
+                    }
+                }
+
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
